27549e7064b6b9db1a983879e6c55467d60d1a22
dan <danieldv2283@gmail.com>
2026-03-04 10:16:04 -0400

    feat[firstPage]: added the first page for content nav

--- src/components/FirstPage.tsx
+++ src/components/FirstPage.tsx
@@ -1,15 +1,22 @@
+import LogoIcon from "../assets/icons/logo-icon.svg";
+import styles from "./FirstPage.module.css";
+import Button from "../ui/button/Button";
+
 export const FirstPage = () => {
   return (
-    <p
-      style={{
-        margin: 0,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100%",
-      }}
-    >
-      pag 1
-    </p>
+    <div className={styles.wrapper}>
+      <img src={LogoIcon.src} className={styles.logo} loading="lazy" />
+      <h2 className={styles.title}>
+        Desarrollo, diseño y estrategia en un solo universo
+      </h2>
+      <Button
+        customClass={styles.button}
+        text="AGENDAR UNA REUNIÓN"
+        variant="primary"
+        onClick={() => {
+          console.log("agendando");
+        }}
+      />
+    </div>
   );
 };
